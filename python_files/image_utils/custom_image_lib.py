4acbbb77161072206334ede615fa5fa15cd285bd
SlavaInder <v.inderiakin.uk@gmail.com>
2020-03-19 16:18:12 -0700

    finally fixed folding second time

--- python_files/image_utils/custom_image_lib.py
+++ python_files/image_utils/custom_image_lib.py
@@ -163,19 +163,19 @@ def sample_dot(size, thickness, side):
     # top
     if side == 0:
         y = size
-        x = np.random.randint(thickness+1, size-thickness-1)
+        x = np.random.randint(thickness+2, size-thickness-2)
     # right
     elif side == 1:
         x = size
-        y = np.random.randint(thickness+1, size-thickness-1)
+        y = np.random.randint(thickness+2, size-thickness-2)
     # bottom
     elif side == 2:
         y = 0
-        x = np.random.randint(thickness+1, size-thickness-1)
+        x = np.random.randint(thickness+2, size-thickness-2)
     # left
     else:
         x = 0
-        y = np.random.randint(thickness+1, size-thickness-1)
+        y = np.random.randint(thickness+2, size-thickness-2)
 
     return x, y
 
